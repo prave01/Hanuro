0769a671b09670a5099988cd8d2a613c37748984
Praveen Thanikachalam <praveenthanikachalam408@gmail.com>
2025-08-04 18:29:35 +0530

    Inital Commit

---
+++ apps/electron-app/src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): React.JSX.Element {
+  return (
+    <div className="h-screen w-screen bg-zinc-900 text-2xl font-medium text-orange-200">
+      New to Electron Js
+    </div>
+  );
+}
+
+export default App;
